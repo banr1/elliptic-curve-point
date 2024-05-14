1ff48abe806ac257d571e40add930795936842b4
banri <banri.yanahama@gmail.com>
2024-05-14 13:48:04 +0900

    add displaying elliptic curve points

--- src/elliptic_curve_point/__init__.py
+++ src/elliptic_curve_point/__init__.py
@@ -1,2 +1,9 @@
-def hello() -> str:
-    return "Hello from elliptic-curve-point!"
+def display_elliptic_curve_points(a: int, b: int, p: int) -> None:
+    for x in range(p):
+        for y in range(p):
+            if (y ** 2) % p == (x ** 3 + a * x + b) % p:
+                print(f"({x}, {y})")
+
+def main() -> int:
+    display_elliptic_curve_points(0, 7, 11)
+    return 0
---
+++ src/elliptic_curve_point/__main__.py
@@ -0,0 +1,4 @@
+import elliptic_curve_point
+import sys
+
+sys.exit(elliptic_curve_point.main())
